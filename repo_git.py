a8e19365e2032f32cbc95e837e0b2876573e5fd5
Laureano <escoca.laureano09@gmail.com>
2025-03-26 18:20:27 -0300

    se agregaron comentarios a ejercicio1

--- repo_git.py
+++ repo_git.py
@@ -1,4 +1,8 @@
+# Se defiene variable nombre
 nombre = "Laureano"
+# Se defiene variable apellido
 apellido = "Escoca"
+# Se defiene variable materia
 materia = "Programación 1"
+# Se imprime mensaje por consola
 print(f"Hola soy {nombre} {apellido} alumno de la materia {materia}")
